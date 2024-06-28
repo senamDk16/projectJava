ee91c84b6cd9c28c8469913f9e3dc4f7fd3c3738
senamDk16 <123488280+senamDk16@users.noreply.github.com>
2024-06-28 17:57:25 +0000

    Add files via upload
    addd

---
+++ stg/src/stg/Stg.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stg;
+
+import Introduction.FormStagaire;
+
+/**
+ *
+ * @author Senam
+ */
+public class Stg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new FormStagaire().setVisible(true);
+    }
+
+    
+}
